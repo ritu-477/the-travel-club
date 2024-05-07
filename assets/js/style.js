c3d31288bc99bf5686624089b9e825b7b0a8cccd
ritu-477 <ritujoharbishnoi@gmail.com>
2024-05-07 09:03:49 +0530

    header-section done with responsive

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,3 +3,34 @@ function openNav() {
     document.body.classList.toggle("overflowhidden")
     document.querySelector(".iconbox").classList.toggle("cross")
 }
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+let currentDate = new Date(2024, 5, 29);
+function updateDate() {
+    const monthName = months[currentDate.getMonth()];
+    const day = currentDate.getDate();
+    document.getElementById("date").textContent = monthName + " " + day;
+}
+document.getElementById("prew").addEventListener("click", function () {
+    currentDate.setDate(currentDate.getDate() + 1);
+    updateDate();
+});
+document.getElementById("next").addEventListener("click", function () {
+    currentDate.setDate(currentDate.getDate() - 1);
+    updateDate();
+});
+
+updateDate();
