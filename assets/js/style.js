90f9cee4d9ebad4e0e3d8384216938c69a35bce0
ritu-477 <ritujoharbishnoi@gmail.com>
2024-05-11 09:14:11 +0530

    member-deal section done

--- assets/js/style.js
+++ assets/js/style.js
@@ -34,3 +34,53 @@ document.getElementById("next").addEventListener("click", function () {
 });
 
 updateDate();
+
+$('.responsive1').slick({
+    dots: true,
+    infinite: true,
+    speed: 200,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: ".next-arrow",
+    prevArrow: ".prew-arrow",
+    responsive: [
+        {
+            breakpoint: 1440,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+
+            }
+
+        },
+
+    ]
+});
